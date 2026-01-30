5e417adb8db6857535898f8ac8bca56797b0a0fe
Yash Goswami <yashgoswamiyg2003@gmail.com>
2026-01-31 05:11:49 +0530

    correcting

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,7 @@ import Footer from '@/components/Portfolio/Footer'
 import AuthorLinks from '@/components/Portfolio/AuthorLinks'
 import { getPortfolioConfig } from '@/lib/portfolioConfig'
 
-export const dynamic = 'force-dynamic'
+export const revalidate = 60;
 
 export default async function About() {
     const data = await getPortfolioConfig()
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,7 @@
 import { getPortfolioConfig } from '@/lib/portfolioConfig'
 import ProjectsPageClient from '@/components/Portfolio/ProjectsPageClient'
 
-export const dynamic = 'force-dynamic'
+export const revalidate = 60;
 
 export default async function Projects() {
   const data = await getPortfolioConfig()
