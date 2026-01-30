ca5e240ee5feae3144eec0b3ac74ab8ffa06369a
Yash Goswami <yashgoswamiyg2003@gmail.com>
2026-01-31 05:02:02 +0530

    Security fix update

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = "mongodb+srv://yashgoswamiyg2003:yash1234@cluster0.mttpnyr.mongodb.net/";
+const uri = process.env.MONGO;
 const options = {};
 
 let client;
